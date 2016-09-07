1bf6a67aaca02ace786e032cc4ea45acb27f84d6
lipu <lipu@tingyun.com>
2016-09-07 16:45:59 +0800

    修改iostat，解决centos7和6结果不一致问题

--- iostats/iostats.sh
+++ iostats/iostats.sh
@@ -10,17 +10,17 @@ ZBX_SENDER=/opt/zabbix/bin/zabbix_sender
 function sender {
     $ZBX_SENDER -c $ZBX_CONF -s ${HOSTNAME} -k $1 -o $2 &>/dev/null
 }
-result=$(iostat -x 1 2 |grep $dev |tail -1)
+result=$(iostat -xmt 1 2 |grep $dev |tail -1)
 if [[ $result =~ vd|sd ]];then
     rsec=$(echo $result|awk '{print $4}')
     wsec=$(echo $result|awk '{print $5}')
     rKB=$(echo $result|awk '{print $6}')
     wKB=$(echo $result|awk '{print $7}')
-    await=$(echo $result|awk '{print $10}')
-    svctm=$(echo $result|awk '{print $13}')
-    util=$(echo $result|awk '{print $14}')
+    await=$(echo $result|awk '{print $(NF-2)}')
+    svctm=$(echo $result|awk '{print $(NF-1)}')
+    util=$(echo $result|awk '{print $NF}')
     for key in rsec wsec rKB wKB await svctm util;do
-        sender iostats[$dev,$key] $(eval echo \${$key})
+    echo    sender iostats[$dev,$key] $(eval echo \${$key})
     done
     echo Succeeded
 else
